sharetribe/web-template

Separate upload progress and verification progress

src/components/FileUpload/FileUpload.js
@@ -88,22 +88,31 @@ const IconCross = () => (
 
 const FileUpload = props => {
   const { item, className, onRemoveFile } = props;
-  const { file, tempId, progress, inProgress, sourceFile, error, verificationStatus } = item;
+  const {
+    file,
+    tempId,
+    progress,
+    uploadInProgress,
+    verificationInProgress,
+    sourceFile,
+    error,
+  } = item;
   const intl = useIntl();
 
   const name = file?.attributes?.name ?? sourceFile?.name;
 
-  const isPendingFile = inProgress && !file;
-  const hasError = !inProgress && error && sourceFile;
-  const hasCompletedUpload = file && !inProgress && !error;
+  const hasError = !uploadInProgress && !verificationInProgress && error && (sourceFile || file);
+  const hasCompletedUpload = !uploadInProgress && !error;
 
-  const isKnownState = isPendingFile || hasError || hasCompletedUpload;
+  const isKnownState =
+    uploadInProgress || verificationInProgress || hasError || (hasCompletedUpload && file);
   if (!isKnownState) {
     return null;
   }
 
   let statusIcon;
-  if (isPendingFile) {
+  if (uploadInProgress || verificationInProgress) {
+    // TODO include progress functionality
     statusIcon = <IconSpinner />;
   } else if (hasError) {
     statusIcon = <IconError />;
@@ -112,13 +121,22 @@ const FileUpload = props => {
   }
 
   let statusText;
-  if (isPendingFile && progress < 100) {
+  if (uploadInProgress) {
     statusText = <FormattedMessage id="FileUpload.uploading" />;
-  } else if (isPendingFile && progress === 100 && verificationStatus !== 'available') {
+  } else if (verificationInProgress) {
     statusText = <FormattedMessage id="FileUpload.verifying" />;
   } else if (hasError) {
-    statusText = error.message || <FormattedMessage id="FileUpload.uploadFailed" />;
-  } else if (hasCompletedUpload) {
+    statusText =
+      error.reason === 'verificationFailed' ? (
+        <FormattedMessage id="FileUpload.verificationFailed" />
+      ) : error.reason === 'timeout' ? (
+        <FormattedMessage id="FileUpload.verificationFailed" />
+      ) : error.message ? (
+        error.message
+      ) : (
+        <FormattedMessage id="FileUpload.uploadFailed" />
+      );
+  } else if (hasCompletedUpload && !verificationInProgress) {
     const { size: sizeRaw } = file?.attributes;
     const { size, unit } = calculateFileSize(sizeRaw);
     statusText = `${size} ${unit}`;

src/containers/TransactionPage/SendMessageForm/SendMessageForm.example.js
@@ -62,8 +62,9 @@ export const WithFiles = {
     },
     files: [
       {
-        tempId: 'example-in-progress',
-        inProgress: true,
+        tempId: 'example-uploading',
+        uploadInProgress: true,
+        verificationInProgress: false,
         file: null,
         sourceFile: { name: 'presentation.pdf' },
         progress: 60,
@@ -71,8 +72,19 @@ export const WithFiles = {
         verificationStatus: null,
       },
       {
+        tempId: 'example-verifying',
+        uploadInProgress: false,
+        verificationInProgress: true,
+        file: { attributes: { name: 'contract.pdf', size: 150 * 1024 } },
+        sourceFile: null,
+        progress: 100,
+        error: null,
+        verificationStatus: 'pendingVerification',
+      },
+      {
         tempId: 'example-completed',
-        inProgress: false,
+        uploadInProgress: false,
+        verificationInProgress: false,
         file: { attributes: { name: 'photo.jpg', size: 320 * 1024 } },
         sourceFile: null,
         progress: 100,
@@ -80,13 +92,24 @@ export const WithFiles = {
         verificationStatus: 'available',
       },
       {
-        tempId: 'example-error',
-        inProgress: false,
+        tempId: 'example-upload-error',
+        uploadInProgress: false,
+        verificationInProgress: false,
         file: null,
         sourceFile: { name: 'video.mov' },
         progress: null,
-        error: { message: 'File too large.' },
+        error: { message: 'File too large (max 1 GB).' },
         verificationStatus: null,
+      },
+      {
+        tempId: 'example-verification-failed',
+        uploadInProgress: false,
+        verificationInProgress: false,
+        file: { attributes: { name: 'suspicious.zip', size: 50 * 1024 } },
+        sourceFile: null,
+        progress: 100,
+        error: { reason: 'verificationFailed' },
+        verificationStatus: 'verificationFailed',
       },
     ],
   },

PedroFerr/web-template-jpf-watches

src/components/FileUpload/FileUpload.js
@@ -88,22 +88,31 @@ const IconCross = () => (
 
 const FileUpload = props => {
   const { item, className, onRemoveFile } = props;
-  const { file, tempId, progress, inProgress, sourceFile, error, verificationStatus } = item;
+  const {
+    file,
+    tempId,
+    progress,
+    uploadInProgress,
+    verificationInProgress,
+    sourceFile,
+    error,
+  } = item;
   const intl = useIntl();
 
   const name = file?.attributes?.name ?? sourceFile?.name;
 
-  const isPendingFile = inProgress && !file;
-  const hasError = !inProgress && error && sourceFile;
-  const hasCompletedUpload = file && !inProgress && !error;
+  const hasError = !uploadInProgress && !verificationInProgress && error && (sourceFile || file);
+  const hasCompletedUpload = !uploadInProgress && !error;
 
-  const isKnownState = isPendingFile || hasError || hasCompletedUpload;
+  const isKnownState =
+    uploadInProgress || verificationInProgress || hasError || (hasCompletedUpload && file);
   if (!isKnownState) {
     return null;
   }
 
   let statusIcon;
-  if (isPendingFile) {
+  if (uploadInProgress || verificationInProgress) {
+    // TODO include progress functionality
     statusIcon = <IconSpinner />;
   } else if (hasError) {
     statusIcon = <IconError />;
@@ -112,13 +121,24 @@ const FileUpload = props => {
   }
 
   let statusText;
-  if (isPendingFile && progress < 100) {
+  if (uploadInProgress) {
     statusText = <FormattedMessage id="FileUpload.uploading" />;
-  } else if (isPendingFile && progress === 100 && verificationStatus !== 'available') {
+  } else if (verificationInProgress) {
     statusText = <FormattedMessage id="FileUpload.verifying" />;
   } else if (hasError) {
-    statusText = error.message || <FormattedMessage id="FileUpload.uploadFailed" />;
-  } else if (hasCompletedUpload) {
+    statusText =
+      error.message === 'verificationFailed' ? (
+        <FormattedMessage id="FileUpload.verificationFailed" />
+      ) : error.message === 'mimeTypeError' ? (
+        <FormattedMessage id="FileUpload.mimeTypeError" />
+      ) : error.message === 'timeout' ? (
+        <FormattedMessage id="FileUpload.uploadFailed" />
+      ) : error.message ? (
+        error.message
+      ) : (
+        <FormattedMessage id="FileUpload.uploadFailed" />
+      );
+  } else if (hasCompletedUpload && !verificationInProgress) {
     const { size: sizeRaw } = file?.attributes;
     const { size, unit } = calculateFileSize(sizeRaw);
     statusText = `${size} ${unit}`;

src/containers/TransactionPage/SendMessageForm/SendMessageForm.example.js
@@ -62,8 +62,9 @@ export const WithFiles = {
     },
     files: [
       {
-        tempId: 'example-in-progress',
-        inProgress: true,
+        tempId: 'example-uploading',
+        uploadInProgress: true,
+        verificationInProgress: false,
         file: null,
         sourceFile: { name: 'presentation.pdf' },
         progress: 60,
@@ -71,8 +72,19 @@ export const WithFiles = {
         verificationStatus: null,
       },
       {
+        tempId: 'example-verifying',
+        uploadInProgress: false,
+        verificationInProgress: true,
+        file: { attributes: { name: 'contract.pdf', size: 150 * 1024 } },
+        sourceFile: null,
+        progress: 100,
+        error: null,
+        verificationStatus: 'pendingVerification',
+      },
+      {
         tempId: 'example-completed',
-        inProgress: false,
+        uploadInProgress: false,
+        verificationInProgress: false,
         file: { attributes: { name: 'photo.jpg', size: 320 * 1024 } },
         sourceFile: null,
         progress: 100,
@@ -80,12 +92,43 @@ export const WithFiles = {
         verificationStatus: 'available',
       },
       {
-        tempId: 'example-error',
-        inProgress: false,
+        tempId: 'example-mime-type-error',
+        uploadInProgress: false,
+        verificationInProgress: false,
+        file: null,
+        sourceFile: { name: 'script.exe' },
+        progress: null,
+        error: { message: 'mimeTypeError' },
+        verificationStatus: null,
+      },
+      {
+        tempId: 'example-verification-failed',
+        uploadInProgress: false,
+        verificationInProgress: false,
+        file: { attributes: { name: 'suspicious.zip', size: 50 * 1024 } },
+        sourceFile: null,
+        progress: 100,
+        error: { message: 'verificationFailed' },
+        verificationStatus: 'verificationFailed',
+      },
+      {
+        tempId: 'example-timeout',
+        uploadInProgress: false,
+        verificationInProgress: false,
         file: null,
         sourceFile: { name: 'video.mov' },
         progress: null,
-        error: { message: 'File too large.' },
+        error: { message: 'timeout' },
+        verificationStatus: null,
+      },
+      {
+        tempId: 'example-upload-error',
+        uploadInProgress: false,
+        verificationInProgress: false,
+        file: null,
+        sourceFile: { name: 'archive.zip' },
+        progress: null,
+        error: { message: 'File too large (max 1 GB).' },
         verificationStatus: null,
       },
     ],