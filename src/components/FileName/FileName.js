sharetribe/web-template

Implement FileName component with long word truncation

src/components/FileName/FileName.js
@@ -1,0 +1,16 @@
+import React from 'react';
+import { analyseFileName } from '../../util/fileHelpers';
+
+import css from './FileName.module.css';
+
+const FileName = ({ name, className }) => {
+  const { baseName, extension } = analyseFileName(name);
+  return (
+    <span className={className || css.root}>
+      <span className={css.baseName}>{baseName}</span>
+      {extension ? <span className={css.extension}>{extension}</span> : null}
+    </span>
+  );
+};
+
+export default FileName;

PedroFerr/web-template-jpf-watches

src/components/FileName/FileName.js
@@ -1,0 +1,16 @@
+import React from 'react';
+import { analyseFileName } from '../../util/fileHelpers';
+
+import css from './FileName.module.css';
+
+const FileName = ({ name, className }) => {
+  const { baseName, extension } = analyseFileName(name);
+  return (
+    <span className={className || css.root}>
+      <span className={css.dynamicNameBeginning}>{baseName}</span>
+      {extension ? <span className={css.fixedNameEnding}>{extension}</span> : null}
+    </span>
+  );
+};
+
+export default FileName;